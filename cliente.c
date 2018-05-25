bac667dd5450da9cd656c79fa8de6711a021b43f
Rodrigo Costa <svork@users.noreply.github.com>
2018-05-24 21:45:47 -0300

    Correções no cliente

--- cliente.c
+++ cliente.c
@@ -19,13 +19,157 @@
 // Biblioteca Padrão de Entrada/Saída de dados
 #include <stdio.h>
 
+// Print na mesma posição, algo como um clear screen, dica do Tonight
+void limpar(void) {
+  printf("\e[2J\e[H");
+}
+
+// Função inicio - exibe a tela inicial, assim que o cliente conectar
+void inicio(void) {
+  printf ("\n");
+  printf (" █████╗ ███╗   ███╗███████╗███╗   ██╗    ███████╗ ██████╗  ██████╗ ██████╗ ███████╗\n");
+  printf ("██╔══██╗████╗ ████║╚══███╔╝████╗  ██║    ██╔════╝██╔═══██╗██╔═══██╗██╔══██╗██╔════╝\n");
+  printf ("███████║██╔████╔██║  ███╔╝ ██╔██╗ ██║    █████╗  ██║   ██║██║   ██║██║  ██║███████╗\n");
+  printf ("██╔══██║██║╚██╔╝██║ ███╔╝  ██║╚██╗██║    ██╔══╝  ██║   ██║██║   ██║██║  ██║╚════██║\n");
+  printf ("██║  ██║██║ ╚═╝ ██║███████╗██║ ╚████║    ██║     ╚██████╔╝╚██████╔╝██████╔╝███████║\n");
+  printf ("╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝    ╚═╝      ╚═════╝  ╚═════╝ ╚═════╝ ╚══════╝\n");
+  printf ("\n");
+  printf("(0) - Fazer pedido\n");
+  printf("(1) - Sair\n");
+  printf ("\n");
+}
+
+// Função escolher_nome - pede para o cliente digitar o nome
+void escolher_nome(void) {
+  printf("=======================================================================\n");
+  printf("\n");
+  printf("Digite seu nome: \n");
+  printf("\n");
+}
+
+// Função escolher_comida - pede para o cliente digitar o número do prato
+void escolher_comida(void) {
+  printf("=======================================================================\n");
+  printf("\n");
+  printf("(0) - Marinado de salmão com lentilha puy\n");
+  printf("(1) - Peixe à grega com tomates marinados\n");
+  printf("(2) - Pescada amarela ao forno com purê de berinjela\n");
+  printf("(3) - Camarão ao tamarindo e leite de coco\n");
+  printf("(4) - Arroz de polvo com chips de alho-poró\n");
+  printf("(5) - Polvo com páprica e purê de batata-doce\n");
+  printf("(6) - Medalhões de lagosta e salada\n");
+  printf("(7) - Siri mole com maionese de alga nori\n");
+  printf("(8) - Empanada de banana-verde com recheio de siri\n");
+  printf("(9) - Hambúrguer de salmão com maionese de lima da pérsia\n");
+  printf("\n");
+  printf("Digite o numero desejado: \n");
+  printf("\n");
+}
+
+// Função escolher_bebida - pede para o cliente digitar o número da bebida
+void escolher_bebida(void) {
+  printf("=======================================================================\n");
+  printf("\n");
+  printf("(0) - Vinho Azul\n");
+  printf("(1) - Catuaba com açaí\n");
+  printf("(2) - Vinho peçonhento\n");
+  printf("(3) - Golden Coconut\n");
+  printf("(4) - Cerveja de Chá Verde\n");
+  printf("(5) - Whisky de ouro\n");
+  printf("(6) - Vinho para gatos\n");
+  printf("(7) - Suco de Farinha de Banana Verde\n");
+  printf("\n");
+  printf("Digite o numero desejado: \n");
+  printf("\n");
+}
+
+// Função escolher_pagamento - pede para o cliente digitar o número da forma de pagamento
+void escolher_pagamento(void) {
+  printf("=======================================================================\n");
+  printf("\n");
+  printf("0) - Dinheiro\n");
+  printf("1) - Cartão de Crédito\n");
+  printf("2) - Cripto Moedas\n");
+  printf("\n");
+  printf("Digite o numero desejado: \n");
+  printf("\n");
+}
+
+// Função confirmar_pedido - exibe os detalhes do pedido do cliente
+void confirmar_pedido(void) {
+  printf("=======================================================================\n");
+  printf ("Cliente %s", nome,", seu pedido foi: %d e um %d ", comida, bebida,", e a forma de pagamento escolhida foi: %d", pagamento);
+  printf ("\nSeu pedido será entregue em: 10 minutos.\n");
+  printf ("Após 15 segundos essa mensagem se auto-destruirá.\n");
+}
+
+// Função escolher_tela - recebe um inteiro e exibe a tela escolhida
+void escolher_tela(int numero) {
+  switch (numero) {
+    case 0 :
+      // Tela Inicial
+      inicio();
+      break;
+
+    case 1 :
+      // Número 1 sai do sistema
+      exit(1);
+      break;
+
+    case 2 :
+      // Escolher Nome
+      escolher_nome();
+      break;
+
+    case 3 :
+      // Escolher Comida
+      escolher_comida();
+      break;
+
+    case 4 :
+      // Escolher Bebida
+      escolher_bebida();
+      break;
+
+    case 5 :
+      // Escolher Pagamento
+      escolher_pagamento();
+      break;
+
+    case 6 :
+      // Confirmar Pedido
+      confirmar_pedido();
+      break;
+
+    default :
+      // Mensagem de erro
+      printf("Opcao Invalida\n");
+      break;
+  }
+}
+
 // Função Main
 int main(int argc, char **argv) {
   struct sockaddr_in me, server;
   int adl=sizeof(me);
 
   // Vetor linha contem o texto a ser enviado para o servidor
-  char linha[2000];
+  char linha[80];
+
+  // Variável tela define qual será enviada para o cliente
+  int tela = 0;
+
+  // Variável nome guarda o nome do cliente
+  char nome[20];
+
+  // Variável comida guarda o número do prato escolhido
+  int comida = 0;
+
+  // Variável bebida guarda o número da bebida escolhido
+  int bebida = 0;
+
+  // Variável pagamento guarda o número do pagamento escolhido
+  int pagamento = 0;
 
   // Variáveis Windows
   #ifdef WIN32
@@ -61,7 +205,6 @@ int main(int argc, char **argv) {
 
     // Sair do programa
 		exit(1);
-
 	}
     
   // Alguma definição Windows ( ainda não sei o que é isso )
@@ -90,14 +233,23 @@ int main(int argc, char **argv) {
   // Aqui as mensagens serão enviadas e recebidas, até que o usuário digite "sair"
   do {
 
+    // Mostrar a tela
+    escolher_tela(tela);
+
     // Ler o que o usuário digitar
     gets(linha);
+
+    // Limpar a tela
+    limpar();
     
     // Enviar o texto para o servidor
-    sendto(sock,linha,2000,0,(struct sockaddr *)&server,adl);
+    sendto(sock,linha,80,0,(struct sockaddr *)&server,adl);
+
+    // Exibir a próxima tela
+    //tela++;
 
     // Receber o texto do servidor
-    recvfrom(sock,linha,2000,0,(struct sockaddr *)&server,&adl);
+    recvfrom(sock,linha,80,0,(struct sockaddr *)&server,&adl);
 
     // Escrever na tela o que o servidor mandar
     puts(linha);
--- servidor.c
+++ servidor.c
@@ -25,7 +25,7 @@ int main(void) {
 	int adl=sizeof(me);
 
   // Vetor linha contém o texto a ser enviado para o cliente
-	char linha[2000];
+  char linha[80];
 
   // Variável tela define qual será enviada para o cliente
   int tela = 0;
@@ -41,131 +41,6 @@ int main(void) {
 
   // Variável pagamento guarda o número do pagamento escolhido
   int pagamento = 0;
-
-  // Função inicio - exibe a tela inicial, assim que o cliente conectar
-  void inicio() {
-    printf ("\n");
-    printf (" █████╗ ███╗   ███╗███████╗███╗   ██╗    ███████╗ ██████╗  ██████╗ ██████╗ ███████╗\n");
-    printf ("██╔══██╗████╗ ████║╚══███╔╝████╗  ██║    ██╔════╝██╔═══██╗██╔═══██╗██╔══██╗██╔════╝\n");
-    printf ("███████║██╔████╔██║  ███╔╝ ██╔██╗ ██║    █████╗  ██║   ██║██║   ██║██║  ██║███████╗\n");
-    printf ("██╔══██║██║╚██╔╝██║ ███╔╝  ██║╚██╗██║    ██╔══╝  ██║   ██║██║   ██║██║  ██║╚════██║\n");
-    printf ("██║  ██║██║ ╚═╝ ██║███████╗██║ ╚████║    ██║     ╚██████╔╝╚██████╔╝██████╔╝███████║\n");
-    printf ("╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝    ╚═╝      ╚═════╝  ╚═════╝ ╚═════╝ ╚══════╝\n");
-    printf ("\n");
-    printf("(0) - Fazer pedido\n");
-    printf("(1) - Sair\n");
-    printf ("\n");
-  }
-
-  // Função escolher_nome - pede para o cliente digitar o nome
-  void escolher_nome() {
-    printf("=======================================================================\n");
-    printf("\n");
-    printf("Digite seu nome: \n");
-    printf("\n");
-  }
-
-  // Função escolher_comida - pede para o cliente digitar o número do prato
-  void escolher_comida() {
-    printf("=======================================================================\n");
-    printf("\n");
-    printf("(0) - Marinado de salmão com lentilha puy\n");
-    printf("(1) - Peixe à grega com tomates marinados\n");
-    printf("(2) - Pescada amarela ao forno com purê de berinjela\n");
-    printf("(3) - Camarão ao tamarindo e leite de coco\n");
-    printf("(4) - Arroz de polvo com chips de alho-poró\n");
-    printf("(5) - Polvo com páprica e purê de batata-doce\n");
-    printf("(6) - Medalhões de lagosta e salada\n");
-    printf("(7) - Siri mole com maionese de alga nori\n");
-    printf("(8) - Empanada de banana-verde com recheio de siri\n");
-    printf("(9) - Hambúrguer de salmão com maionese de lima da pérsia\n");
-    printf("\n");
-    printf("Digite o numero desejado: \n");
-    printf("\n");
-  }
-
-  // Função escolher_bebida - pede para o cliente digitar o número da bebida
-  void escolher_bebida() {
-    printf("=======================================================================\n");
-    printf("\n");
-    printf("(0) - Vinho Azul\n");
-    printf("(1) - Catuaba com açaí\n");
-    printf("(2) - Vinho peçonhento\n");
-    printf("(3) - Golden Coconut\n");
-    printf("(4) - Cerveja de Chá Verde\n");
-    printf("(5) - Whisky de ouro\n");
-    printf("(6) - Vinho para gatos\n");
-    printf("(7) - Suco de Farinha de Banana Verde\n");
-    printf("\n");
-    printf("Digite o numero desejado: \n");
-    printf("\n");
-  }
-
-  // Função escolher_pagamento - pede para o cliente digitar o número da forma de pagamento
-  void escolher_pagamento() {
-    printf("=======================================================================\n");
-    printf("\n");
-    printf("0) - Dinheiro\n");
-    printf("1) - Cartão de Crédito\n");
-    printf("2) - Cripto Moedas\n");
-    printf("\n");
-    printf("Digite o numero desejado: \n");
-    printf("\n");
-  }
-
-  // Função confirmar_pedido - exibe os detalhes do pedido do cliente
-  void confirmar_pedido() {
-    printf("=======================================================================\n");
-    printf ("Cliente %s",nome,", seu pedido foi: ",comida,bebida,", e a forma de pagamento escolhida foi: ",pagamento);
-    printf ("\nSeu pedido será entregue em: 10 minutos.\n");
-    printf ("Após 15 segundos essa mensagem se auto-destruirá.\n");
-  }
-
-  // Função escolher_tela - recebe um inteiro e exibe a tela escolhida
-  void escolher_tela(int numero) {
-    switch (numero) {
-      case 0 :
-        // Tela Inicial
-        inicio();
-        break;
-
-      case 1 :
-        // Número 1 sai do sistema
-        //exit(1);
-        break;
-
-      case 2 :
-        // Escolher Nome
-        escolher_nome();
-        break;
-
-      case 3 :
-        // Escolher Comida
-        escolher_comida();
-        break;
-
-      case 4 :
-        // Escolher Bebida
-        escolher_bebida();
-        break;
-
-      case 5 :
-        // Escolher Pagamento
-        escolher_pagamento();
-        break;
-
-      case 6 :
-        // Confirmar Pedido
-        confirmar_pedido();
-        break;
-
-      default :
-        // Mensagem de erro
-        printf("Opcao Invalida\n");
-        break;
-    }
-  }
-
   
   // Variáveis Windows
   #ifdef WIN32
@@ -203,13 +78,10 @@ int main(void) {
 	  do {
         
         // Receber o que o cliente enviar
-        recvfrom(sock,linha,2000,0,(struct sockaddr *)&client,&adl);
-
-        // Mostrar a tela
-        escolher_tela(tela);
+        recvfrom(sock,linha,80,0,(struct sockaddr *)&client,&adl);
 
         // Enviar para o cliente a resposta
-        sendto(sock,linha,2000,0,(struct sockaddr *)&client,adl);
+        sendto(sock,linha,80,0,(struct sockaddr *)&client,adl);
         
         // Exibir a próxima tela
         tela++;
